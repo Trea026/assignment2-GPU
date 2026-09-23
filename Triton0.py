248b397b5d76bc4956417d12bc383976d9229190
xiaoyan <zhangxiaoyan@xiaoyandeMacBook-Air.local>
2026-09-23 11:57:05 +0800

    fix

--- Triton0.py
+++ Triton0.py
@@ -10,11 +10,11 @@ def add_kernel(x_ptr,y_ptr,output_ptr,n_elements,BLOCK_SIZE:tl.constexpr):
     pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0,BLOCK_SIZE)
-    #mask =
-    x = tl.load(x_ptr + offsets)
-    y = tl.load(y_ptr + offsets)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets,mask=mask)
+    y = tl.load(y_ptr + offsets,mask=mask)
     output = x + y 
-    tl.store(output_ptr + offsets ,output)
+    tl.store(output_ptr + offsets ,output,mask=mask)
 #—— wrapper #———
 def add(x:torch.Tensor, y:torch.Tensor):
     out = torch.empty_like(x)
